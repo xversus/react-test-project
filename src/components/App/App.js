bad8e83e1fc01250c1aa17b71247a7f04c964e85
Nikita Strashnov <xversus.nick@gmail.com>
2019-03-26 17:36:34 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="app-page">
+      <div className="app-container" />
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./public",
+    hot: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
